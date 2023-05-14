cb2678c5308b115d721e3d8692867df8b27263ea
TheWhiteMachine <priveiro@gmail.com>
2023-05-14 11:29:46 -0700

    fix for mixing declaration and code

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -15,8 +15,10 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		if (binary_tree_is_root(tree) == 1)
 			return (0);
 
-		int left_depth = binary_tree_depth(tree->left);
-		int right_depth = binary_tree_depth(tree->right);
+		int left_depth, right_depth;
+
+		left_depth = binary_tree_depth(tree->left);
+		right_depth = binary_tree_depth(tree->right);
 
 		if (left_depth > right_depth)
 			return (left_depth + 1);
