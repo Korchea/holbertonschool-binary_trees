c437544c01d6521f3b0632e0b2aa9e55092bd7a2
TheWhiteMachine <priveiro@gmail.com>
2023-05-14 11:05:57 -0700

    10 depth

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_deptht - 'calculate the depth of a given tree'
+ * @tree: Is a pointer to the root node of a tree
+ * Return: depth from the root to a node given
+ */
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return -1;
+
+	int left_depth = binary_tree_depth(tree->left);
+	int right_depth = binary_tree_depth(tree->right);
+
+	if (left_depth > right_depth)
+		return left_depth + 1;
+	else
+		return right_depth + 1;
+}
