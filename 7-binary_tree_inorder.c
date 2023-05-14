862bb331cc19676f8073d3b25da96b40b214f4ab
TheWhiteMachine <priveiro@gmail.com>
2023-05-14 09:10:10 -0700

    fixing order for 7 in order

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - 'Goes through a binary
+ * binary_tree_inorder - 'Goes through a binary
  * tree using in-order traversal.'
  * @tree: Is a pointer to the root node of the tree to traverse.
  * @func: Is a pointer to a function to call for each node.
@@ -13,6 +13,6 @@ void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 		return;
 
 	binary_tree_inorder(tree->left, func);
-	binary_tree_inorder(tree->right, func);
 	func(tree->n);
+	binary_tree_inorder(tree->right, func);
 }
