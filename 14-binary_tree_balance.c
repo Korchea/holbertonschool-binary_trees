b9814e9198cf99e7bff4a4b5e55ad9bf42f51104
Korchea <6334@holbertonstudents.com>
2023-05-15 10:18:55 -0700

    Task 14 completed

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,29 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - 'calculate the balance of a given tree'
+ * @tree: Is a pointer to the root node of a tree
+ * Return: depth from the root to a node given
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_balance = 0, right_balance = 0;
+
+	if (tree == NULL)
+		return (0);
+	else
+	{
+        if (tree->left != NULL)
+        {
+		    left_balance = binary_tree_balance(tree->left);
+            left_balance++;
+        }
+        if (tree->right != NULL)
+		{
+            right_balance = binary_tree_balance(tree->right);
+            right_balance++;
+        }
+        return (left_balance - right_balance);
+	}
+}
