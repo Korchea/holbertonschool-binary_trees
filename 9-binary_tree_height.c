d879b376f5323fa41628e77e12def630d14cea38
TheWhiteMachine <priveiro@gmail.com>
2023-05-16 08:27:38 -0700

    9 output 2 try

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -16,6 +16,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	}
 	else
 	{
+		if (!left_height && !right_height)
+			return (0);
 		left_height = binary_tree_height(tree->left);
 		right_height = binary_tree_height(tree->right);
 		if (left_height == 1 && left_height == 1)
