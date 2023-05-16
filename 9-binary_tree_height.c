c839212439e3ede7665900b463d0542b38d94bb7
TheWhiteMachine <priveiro@gmail.com>
2023-05-16 08:14:07 -0700

    type on 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -21,6 +21,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		if (left_height > right_height)
 			return (left_height);
 		else
-			return (right_heigh1);
+			return (right_height);
 	}
 }
