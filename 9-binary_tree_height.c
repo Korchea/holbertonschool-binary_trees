894ab28485aa8d295c0bfb64284ce6be5be56564
TheWhiteMachine <priveiro@gmail.com>
2023-05-16 08:40:52 -0700

    backing to prev 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,14 +8,24 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height;
-	size_t right_height;
+	int left_height, right_height;
 
 	if (tree == NULL)
-		return 0;
+	{
+		return (0);
+	}
+	else
+	{
+		if (!tree->left && !tree->right)
+			return (0);
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+		if (left_height == 1 && left_height == 1)
+			return (1);
+		if (left_height > right_height)
+			return (left_height + 1);
+		else
+			return (right_height + 1);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	return (left_height > right_height ? left_height : right_height) + 1;
-}
+		return (left_height);
+	}
