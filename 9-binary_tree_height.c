e776964aa47e450b28dbe83b3f89f925d6310881
TheWhiteMachine <priveiro@gmail.com>
2023-05-16 08:38:08 -0700

    different 9 test 3

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return 0;
-
 	size_t left_height;
 	size_t right_height;
+
+	if (tree == NULL)
+		return 0;
 
 	left_height = binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
