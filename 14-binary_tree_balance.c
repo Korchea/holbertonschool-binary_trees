defccc6cf72b7fc5a0a79e94b9946dae5407a024
TheWhiteMachine <priveiro@gmail.com>
2023-05-16 10:23:23 -0700

    back to first 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -8,29 +8,22 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0, balance_factor = 0;
+	int left_balance = 0, right_balance = 0;
 
 	if (tree == NULL)
 		return (0);
-
-	if (tree->left != NULL)
+	else
 	{
-		left_height = binary_tree_balance(tree->left);
-		if (left_height < 0)
-			return (-1);
+		if (tree->left != NULL)
+		{
+			left_balance = binary_tree_balance(tree->left);
+			left_balance++;
+		}
+		if (tree->right != NULL)
+		{
+			right_balance = binary_tree_balance(tree->right);
+			right_balance++;
+		}
+		return (left_balance - right_balance);
 	}
-
-	if (tree->right != NULL)
-	{
-		right_height = binary_tree_balance(tree->right);
-		if (right_height < 0)
-			return (-1);
-	}
-
-	balance_factor = left_height - right_height;
-
-	if (balance_factor < -1 || balance_factor > 1)
-		return (-1);
-
-	return ((left_height > right_height ? left_height : right_height) + 1);
 }
