229428234ab799f3e28cb5bf54e1291a3725e99b
Korchea <6334@holbertonstudents.com>
2023-05-16 10:39:32 -0700

    Task 9 root = NULL problem solved

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,11 +8,11 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0, right_height = 0;
 
 	if (tree == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 	else
 	{
